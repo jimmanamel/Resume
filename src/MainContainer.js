c16db6dfbb3e3866c6ad3fba6f0c010728c0a8b8
jimmanamel <jimmanamel@gmail.com>
2023-10-13 22:53:44 +0530

    Fixing navbar function

--- src/MainContainer.js
+++ src/MainContainer.js
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import Home from "./Home/Home";
 import NavBar from "./Navigation/NavBar";
 import "./MainContainer.css";
@@ -8,15 +8,44 @@ import Personal from "./Personal/Personal";
 import Contact from "./Contact/Contact";
 
 const MainContainer = () => {
+  const [selectedNav, setSelectedNav] = useState("Home");
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollPosition);
+  }, []);
+  const scrollPosition = () => {
+    if(window.scrollY>=1510){
+      setSelectedNav("Contacts")
+    }
+    else if(window.scrollY>=1300){
+      setSelectedNav("Projects")
+    }
+    else if(window.scrollY>=400){
+      setSelectedNav("Career")
+    }
+    else{
+      setSelectedNav("Home")
+    }
+  };
 
   return (
     <Fragment>
-        <div className="NavBarContainer"><NavBar/></div>
-      <div className="Home"><Home/></div>
+      <div className="NavBarContainer">
+        <NavBar selectedNav={selectedNav} />
+      </div>
+      <div id="Home" className="Home">
+        <Home />
+      </div>
       {/* <div className="About"><About/></div> */}
-      <div className="EduWork"><EduWork/></div>
-      <div className="Projects"><Personal/></div>
-      <div className="Contacts"><Contact/></div>
+      <div id="Career" className="EduWork">
+        <EduWork />
+      </div>
+      <div id="Projects" className="Projects">
+        <Personal />
+      </div>
+      <div id="Contacts" className="Contacts">
+        <Contact />
+      </div>
     </Fragment>
   );
 };
--- src/Navigation/NavBar.js
+++ src/Navigation/NavBar.js
@@ -9,16 +9,37 @@ import { MdOutlineSchool } from "react-icons/md";
 
 import "./NavBar.css";
 
-const NavBar = () => {
+const NavBar = ({selectedNav}) => {
+  console.log(selectedNav)
   return (
-        <div>
-         <div className="dropDownMenuItemInitial"> <AiOutlineHome  size="2em" /></div>
-         {/* <div className="dropDownMenuItem"> <AiOutlineUser  size="2em" /></div> */}
-         <div className="dropDownMenuItem"><MdOutlineSchool  size="2em" /></div>
-         <div className="dropDownMenuItem"><BsWrenchAdjustableCircle  size="2em" /></div>
-         <div className="dropDownMenuItem"><AiOutlineProject  size="2em" /></div>
-         <div className="dropDownMenuItemLast"><AiOutlineContacts  size="2em" /></div>
+    <div>
+      <a href="#Home">
+        <div className="dropDownMenuItemInitial" id={selectedNav==="Home"?"HomeNav":""}>
+          <AiOutlineHome size="2em" />
         </div>
+      </a>
+      {/* <div className="dropDownMenuItem"> <AiOutlineUser  size="2em" /></div> */}
+      <a href="#Career">
+        <div className="dropDownMenuItem" id={selectedNav==="Career"?"CareerNav":""}>
+          <MdOutlineSchool size="2em" />
+        </div>
+      </a>
+      {/* <a href="#Career">
+        <div className="dropDownMenuItem" id={selectedNav==="Career"?"CareerNav":""}>
+          <BsWrenchAdjustableCircle size="2em" />
+        </div>
+      </a> */}
+      <a href="#Projects">
+        <div className="dropDownMenuItem" id={selectedNav==="Projects"?"ProjectsNav":""}>
+          <AiOutlineProject size="2em" />
+        </div>
+      </a>
+      <a href="#Contacts">
+        <div className="dropDownMenuItemLast" id={selectedNav==="Contacts"?"ContactsNav":""}>
+          <AiOutlineContacts size="2em" />
+        </div>
+      </a>
+    </div>
   );
 };
 
