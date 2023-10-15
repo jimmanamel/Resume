e81091eecdb5c162d3cee7dccc60b5e40b53d696
jimmanamel <jimmanamel@gmail.com>
2023-10-15 11:00:01 +0530

    Changing NavBar style

--- src/Navigation/NavBar.js
+++ src/Navigation/NavBar.js
@@ -4,41 +4,35 @@ import {
   AiOutlineProject,
   AiOutlineContacts,
 } from "react-icons/ai";
-import { BsWrenchAdjustableCircle } from "react-icons/bs";
+import { PiDotsNineBold } from "react-icons/pi";
 import { MdOutlineSchool } from "react-icons/md";
 
 import "./NavBar.css";
 
 const NavBar = ({selectedNav}) => {
-  console.log(selectedNav)
   return (
-    <div>
+    <div className="NavOuter">
       <a href="#Home">
-        <div className="dropDownMenuItemInitial" id={selectedNav==="Home"?"HomeNav":""}>
-          <AiOutlineHome size="2em" />
+        <div className="dropDownMenuItem" id={selectedNav==="Home"?"HomeNav":""}>
+          <AiOutlineHome size="2em"/>
         </div>
       </a>
-      {/* <div className="dropDownMenuItem"> <AiOutlineUser  size="2em" /></div> */}
       <a href="#Career">
         <div className="dropDownMenuItem" id={selectedNav==="Career"?"CareerNav":""}>
           <MdOutlineSchool size="2em" />
         </div>
       </a>
-      {/* <a href="#Career">
-        <div className="dropDownMenuItem" id={selectedNav==="Career"?"CareerNav":""}>
-          <BsWrenchAdjustableCircle size="2em" />
-        </div>
-      </a> */}
       <a href="#Projects">
         <div className="dropDownMenuItem" id={selectedNav==="Projects"?"ProjectsNav":""}>
           <AiOutlineProject size="2em" />
         </div>
       </a>
       <a href="#Contacts">
-        <div className="dropDownMenuItemLast" id={selectedNav==="Contacts"?"ContactsNav":""}>
+        <div className="dropDownMenuItem" id={selectedNav==="Contacts"?"ContactsNav":""}>
           <AiOutlineContacts size="2em" />
         </div>
       </a>
+      
     </div>
   );
 };
