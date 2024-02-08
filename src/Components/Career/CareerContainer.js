cad1688ed5d8a49eedd3fdf29d9065cb9f90d83e
jimmanamel <jimmanamel@gmail.com>
2024-02-08 14:37:51 +0530

    latest changes

--- src/Components/Career/CareerContainer.js
+++ src/Components/Career/CareerContainer.js
@@ -1,7 +1,24 @@
-const CareerContainer=()=>{
-    return(
-        <div>This is for Career</div>
-    )
-}
+import "./Career.scss";
 
-export default CareerContainer
+const CareerContainer = () => {
+  return (
+    <div className="careerContainer">
+      <div className="careerHeader">
+        <u>Career Summary</u>
+      </div>
+      <div className="careerInnerContainer">
+        <div className="container1">
+          <div className="professionalCareer">s</div>
+        </div>
+        <div className="container2">
+          <div className="summaryItem">
+            <div className="career2"></div>
+          </div>
+          <div className="summaryItem">t</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CareerContainer;
