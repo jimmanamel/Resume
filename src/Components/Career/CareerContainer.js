7ba5f25a0fd098c62f7316bcc1cebfced1ff2502
jimmanamel <jimmanamel@gmail.com>
2024-02-11 20:05:33 +0530

    updating career page with icon, text, button and basic structure

--- src/Components/Career/CareerContainer.js
+++ src/Components/Career/CareerContainer.js
@@ -1,3 +1,6 @@
+import { FaUserGraduate, FaTools } from "react-icons/fa";
+import { BiBriefcase } from "react-icons/bi";
+
 import "./Career.scss";
 
 const CareerContainer = () => {
@@ -7,14 +10,27 @@ const CareerContainer = () => {
         <u>Career Summary</u>
       </div>
       <div className="careerInnerContainer">
-        <div className="container1">
-          <div className="professionalCareer">s</div>
+        <div className="professionalCareer">
+          <BiBriefcase />
+          <p>Summary of professional career</p>
         </div>
-        <div className="container2">
-          <div className="summaryItem">
-            <div className="career2"></div>
+        <div className="container">
+          <div className="careerCover">
+            <div className="career2">
+              <FaUserGraduate />
+              B.Tech graduate in computer science and engineering, from CEC in
+              the year of 2020
+              <button>Read More</button>
+            </div>
           </div>
-          <div className="summaryItem">t</div>
+          <div className="careerCover">
+            <div className="career3">
+              <FaTools />
+              Has 3 years of Professional experience in variety of font end,
+              backend and full stack technologies
+              <button>Read More</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
