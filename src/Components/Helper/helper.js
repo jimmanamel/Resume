5548fc7d53aa41d6fbd5677ca14a76c91934897e
Jim Joseph <jimmanamel@gmail.com>
2026-01-18 17:16:26 +0530

    Fix resume download issue

--- src/Components/Helper/helper.js
+++ src/Components/Helper/helper.js
@@ -1,12 +1,12 @@
 const handleResumeDownload = () => {
   const link = document.createElement("a");
-  link.href = "/resume.pdf";
+  link.href = `${process.env.PUBLIC_URL}/resume.pdf`;
   link.download = "Jim_Joseph_Resume.pdf";
   link.click();
 };
 
 const normalizeRoute = (route) => {
   return route.replace(/\/$/, "");
-}
+};
 
 export { handleResumeDownload, normalizeRoute };
